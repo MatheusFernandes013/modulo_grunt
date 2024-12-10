cd390b1dce90002dbbafe05ee1f52e577684842d
Matheus <matheus.oliveirag@outlook.com.br>
2024-12-10 13:11:10 -0300

    First Commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,34 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'main.css': 'main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'main.min.css': 'main.less'
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'main2.css': 'main.scss'
+                }
+            }
+        },
+        concurrent: {
+            target: ['olaGrunt', 'less', 'sass']
+        }
     })
 
     grunt.registerTask('olaGrunt', function(){
@@ -10,6 +38,9 @@ module.exports = function(grunt) {
             done();
         }, 3000);
     })
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-concurrent');
 
-    grunt.registerTask('default', ['olaGrunt']);
+    grunt.registerTask('default', ['concurrent']);
 }
